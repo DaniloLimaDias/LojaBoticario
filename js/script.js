8728e4c6fed76be0c001b3e35381cb515a6ddcd0
danilo Lima Dias <danilo743377@gmail.com>
2020-07-13 12:21:58 -0300

    Mudança nas cores e modal

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ fetch('./dados.json')
 .then(json => {
     console.log(json)
     json.map((item, index) =>{
-        let boticarioItem = document.querySelector('.container-item .models .boticario-item').cloneNode(true);
+        let boticarioItem = document.querySelector('.models .boticario-item').cloneNode(true);
 
         boticarioItem.querySelector('.boticario-item--img img').src  = item.images.pop().imageUrl; //tentar entender
         boticarioItem.querySelector('.boticario-item--name').innerHTML = item.name;
     boticarioItem.querySelector('.boticario-item--price').innerHTML = `R$ ${item.Value.toFixed(2)}`;
 
-        document.querySelector('.container-item').append(boticarioItem);
+        document.querySelector('.container-item--area').append(boticarioItem);
     })
 })
